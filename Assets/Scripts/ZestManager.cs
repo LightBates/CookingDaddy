d3b569094a5703fc4337930af91d4968fcc44a18
Kalseng <mqholm@gmail.com>
2019-06-01 13:11:53 -0600

    Zest Minigame Improvements
    Made some UI to help illustrate the current zesting minigame. Worked on displaying the text and altering the answers in accordance with the player-controlled "zestiness".

--- Assets/Scripts/ZestManager.cs
+++ Assets/Scripts/ZestManager.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ZestManager : MonoBehaviour
 {
@@ -8,12 +10,21 @@ public class ZestManager : MonoBehaviour
     private bool playable = true;
     [SerializeField] private ZestableFood foodObject;
     [SerializeField] private Vector3 leftPos, rightPos;
+    [SerializeField] private int zestLevel;
+    [SerializeField] private TextMeshProUGUI zestText;
+
+    [SerializeField] private string[] answers;
+    private string[,] sortedAnswers;
+    private int answerTable;
+
+    [SerializeField] private TextMeshProUGUI answerText;
+    [SerializeField] int secondLevelAnswer, thirdLevelAnswer;
 
     /// <summary>
     /// Current position of the food item.
     /// 0 means left, 1 means right
     /// </summary>
-    [SerializeField]private int foodState = 1;
+    [SerializeField] private int foodState = 1;
 
 
     [SerializeField] private float sprinkleTime = 0f;
@@ -23,7 +34,18 @@ public class ZestManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        sortedAnswers = new string[answers.Length / 3, 3];
+        int counter = 0;
+        foreach(string str in answers)
+        {
+            sortedAnswers[counter / 3, counter % 3] = answers[counter];
+            counter++;
+        }
+
+        answerTable = (int)(Random.value * (counter / 3));
+
+        answerText.text = sortedAnswers[answerTable, 0];
+
     }
 
     // Update is called once per frame
@@ -31,17 +53,17 @@ public class ZestManager : MonoBehaviour
     {
         if (playable)
         {
-            if(Input.GetButtonDown("Left") && foodState == 1)
+            if (Input.GetButtonDown("Left") && foodState == 1)
             {
                 ToggleFoodPos();
             }
-            else if(Input.GetButtonDown("Right") && foodState == 0)
+            else if (Input.GetButtonDown("Right") && foodState == 0)
             {
                 ToggleFoodPos();
             }
         }
 
-        if(sprinkleTime <= 0 )
+        if (sprinkleTime <= 0)
         {
             ps.Stop();
         } else
@@ -50,8 +72,23 @@ public class ZestManager : MonoBehaviour
         }
     }
 
+    void IncreaseZest()
+    {
+        zestLevel++;
+        zestText.text = zestLevel.ToString();
+        if(zestLevel > secondLevelAnswer)
+        {
+            answerText.text = sortedAnswers[answerTable, 1];
+        }
+        if (zestLevel > thirdLevelAnswer)
+        {
+            answerText.text = sortedAnswers[answerTable, 2];
+        }
+    }
+
     void ToggleFoodPos()
     {
+        IncreaseZest();
         if(foodState == 1)
         {
             foodObject.movingTo = leftPos;
